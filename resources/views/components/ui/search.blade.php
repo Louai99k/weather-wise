2cbc206c8b681f22ab8d7be47a3543b4f895c4eb
Louai99k <louaikelani99k@gmail.com>
2025-04-26 15:45:42 +0300

    remove outline from search

--- resources/views/components/ui/search.blade.php
+++ resources/views/components/ui/search.blade.php
@@ -1,6 +1,6 @@
 <div class="relative">
     <input
-        class="w-full border-gray-300 dark:border-gray-500 dark:bg-gray-700 rounded-lg border pl-8 pr-4 py-2 placeholder:text-gray-400"
+        class="w-full outline-0 border-gray-300 dark:border-gray-500 dark:bg-gray-700 rounded-lg border pl-8 pr-4 py-2 placeholder:text-gray-400"
         placeholder="Search for a city...">
     <x-icons.search class="fill-gray-400 absolute translate-y-1/2 bottom-1/2 left-2" />
 </div>
