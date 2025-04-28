0fa5b3ae6c920fe46279a76d900b955089afad6b
Louai99k <louaikelani99k@gmail.com>
2025-04-28 20:39:09 +0300

    save theme pref in local storage

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,18 @@
 window.toggleDarkMode = () => {
-    document.documentElement.classList.toggle("dark");
+    const isDark = document.documentElement.classList.toggle("dark");
+    window.localStorage.setItem("color-scheme", isDark ? "dark" : "light");
 };
+
+const colorScheme = window.localStorage.getItem("color-scheme");
+
+if (!colorScheme) {
+    const isSystemDark =
+        window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    if (isSystemDark) {
+        window.toggleDarkMode();
+    }
+} else if (colorScheme === "dark") {
+    window.toggleDarkMode();
+}
