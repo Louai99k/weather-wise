8120062bcec04eed6b505b889cc4157927ea896a
Louai99k <louaikelani99k@gmail.com>
2025-06-17 17:08:54 +0300

    add htmx and alpine

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,9 +10,13 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
         rel="stylesheet">
     @vite(['resources/js/app.js', 'resources/css/app.css'])
+    <script src="https://unpkg.com/htmx.org@2.0.4"
+        integrity="sha384-HGfztofotfshcF7+8n44JQL2oJmowVChPTg48S+jvZoztPfvwD79OC/LTtG6dMp+" crossorigin="anonymous">
+    </script>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.9/dist/cdn.min.js"></script>
 </head>
 
-<body class="text-txt-first dark:text-white dark:bg-dark-bg">
+<body x-data="{}" class="text-txt-first dark:text-white dark:bg-dark-bg">
     @yield('content')
 </body>
 
